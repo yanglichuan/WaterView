f493f981e7aa13577b4efcdffad39056bc1ddac8
yanglichuan <yanglichuancom@163.com>
2015-12-02 20:18:31 +0800

    瀑布流

---
+++ WaterViewDemo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  WaterViewDemo
+//
+//  Created by zhangbin on 14-8-17.
+//  Copyright (c) 2014年 Z&B. All rights reserved.
+//
+
+// 版权属于原作者
+// http://code4app.com (cn) http://code4app.net (en)
+// 发布代码于最专业的源码分享网站: Code4App.com
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
